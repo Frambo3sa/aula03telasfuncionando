8170acb3970a66d3e562ac508af3914823e2fec9
Frambo3sa <159442304+Frambo3sa@users.noreply.github.com>
2025-04-09 09:25:36 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,111 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { Input, Button, Text } from 'react-native-elements';
+
+const Stack = createStackNavigator();
+
+function telalogin({navigation}) {
+  return (
+    <View style={styles.container}>
+      <Text h3 style={styles.title}>Login</Text>
+     
+      <Avatar
+        size="large"
+        rounded
+        source={{ uri: "https://randomuser.me/api/portraits/men/41.jpg" }}
+      />
+
+      <Input
+      placeholder='Email'
+      leftIcon={{type: 'material', name: 'email'}}
+      keyboardType='email-adress'
+      autoCapitalize='none'
+      />
+       <Input
+      placeholder='senha'
+      leftIcon={{type: 'material', name: 'lock'}}
+      secureTextEntry
+      />
+      <Button
+      title="Cadastrar"
+      buttonStyle= {styles.button}
+      onPress={()=> navigation.navigate('Cadastrar')}
+      />
+      <Button
+      title="entrar"
+      buttonStyle= {styles.button}
+      onPress={()=> alert('Login feito')}
+      />
+      <Button
+      title="Esqueceu a senha"
+      buttonStyle= {styles.button}
+      onPress={()=> navigation.navigate('RecSenha')}
+      />
+    </View>
+  );
+}
+
+
+function Cadastrar({navigation}) {
+  return (
+    <View style={styles.container}>
+      <Text h3 style={styles.title}>Login</Text>
+
+      <Input
+      placeholder='Email'
+      leftIcon={{type: 'material', name: 'email'}}
+      keyboardType='email-adress'
+      autoCapitalize='none'
+      />
+       <Input
+      placeholder='senha'
+      leftIcon={{type: 'material', name: 'lock'}}
+      secureTextEntry
+      />
+      <Button
+      title="cadastre-se"
+      buttonStyle= {styles.button}
+      onPress={()=> navigation.navigate('telalogin')}
+      />
+    </View>
+  );
+}
+
+function RecSenha() {
+  return (
+    <View style={styles.container}>
+      <Text h3 style={styles.title}>RecuperarSenha</Text>
+
+      <Input
+      placeholder='nova senha'
+      leftIcon={{type: 'material', name: 'lock'}}
+      secureTextEntry
+      />
+       <Input
+      placeholder='confirmar nova senha'
+      leftIcon={{type: 'material', name: 'lock'}}
+      secureTextEntry
+      />
+      <Button
+      title="entrar"
+      buttonStyle= {styles.button}
+      onPress={()=> navigation.navigate('telalogin')}
+      />
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="telalogin">
+        <Stack.Screen name="telalogin" component={telalogin} options={{ headerShown: false }} />
+        <Stack.Screen name="Cadastrar" component={Cadastrar} options={{ title: 'Cadastrar' }} />
+        <Stack.Screen name="RecSenha" component={RecSenha} options={{ title: 'RecSenha' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -16,5 +115,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    marginBottom: 30,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  clearButton: {
+    color: 'gray',
   },
 });
